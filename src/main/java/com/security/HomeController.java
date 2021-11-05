1402b9e2e0b75da630415996306a791a26d76f59
DanilSlim <daniilyakovlev76@gmail.com>
2021-11-05 17:02:32 +0300

    Somple Spring Security project

---
+++ src/main/java/com/security/HomeController.java
@@ -0,0 +1,21 @@
+package com.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping({"/","/home"})
+	public String home() {
+		
+		return "home";
+	}
+	
+	@GetMapping("/admin")
+	public String privateHome() {
+		
+		return "privatePage";
+	}
+
+}
